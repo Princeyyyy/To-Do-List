546965918bc3f551f3810315562c981effa057e8
Princeyyyy <princemumo80@gmail.com>
2020-12-09 21:17:39 +0300

    added animation to change from login to register activity and vice versa

--- app/src/main/java/com/example/todolist/LoginActivity.java
+++ app/src/main/java/com/example/todolist/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onRegistrationClick(View View){
+        Intent intent =  new Intent(this,RegistrationActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+
     }
 }
--- app/src/main/java/com/example/todolist/RegistrationActivity.java
+++ app/src/main/java/com/example/todolist/RegistrationActivity.java
@@ -2,7 +2,9 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void onLoginClick(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left,android.R.anim.slide_out_right);
+
     }
 }
