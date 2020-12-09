a1ff6e869c73eb34bc96ef15b7f28f38cd127392
Princeyyyy <princemumo80@gmail.com>
2020-12-10 01:09:20 +0300

    injected tasks to firebase

--- app/src/main/java/com/example/todolist/HomeActivity.java
+++ app/src/main/java/com/example/todolist/HomeActivity.java
@@ -1,11 +1,13 @@
 package com.example.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.ActionMode;
@@ -14,13 +16,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.zip.Inflater;
 
 import butterknife.BindView;
@@ -38,6 +45,7 @@ public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
     private String onlineUserId;
+    private ProgressDialog loader;
 
 
     @Override
@@ -45,6 +53,8 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
+
+        loader = new ProgressDialog(this);
 
 
         toolbar = findViewById(R.id.homeToolbar);
@@ -97,6 +107,8 @@ public class HomeActivity extends AppCompatActivity {
         save.setOnClickListener(v -> {
             String mTask = task.getText().toString().trim();
             String mDescription = description.getText().toString().trim();
+            String id = reference.push().getKey();
+            String date = DateFormat.getDateInstance().format(new Date());
 
             if (TextUtils.isEmpty(mTask)) {
                 task.setError("Task Required");
@@ -106,7 +118,24 @@ public class HomeActivity extends AppCompatActivity {
                 description.setError("Description Required");
                 return;
             } else {
+                loader.setMessage("Adding Your Task");
+                loader.setCanceledOnTouchOutside(false);
+                loader.show();
 
+                Model model = new Model(mTask,mDescription,id,date);
+                reference.child(id).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(HomeActivity.this, "Task Has Been Added Successfully", Toast.LENGTH_SHORT).show();
+                            loader.dismiss();
+                        }else {
+                            String error = task.getException().toString();
+                            Toast.makeText(HomeActivity.this, "Failed " + error, Toast.LENGTH_SHORT).show();
+                            loader.dismiss();
+                        }
+                    }
+                });
             }
             dialog.dismiss();
         });
