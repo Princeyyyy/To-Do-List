0384291eb8183fe477eee5f1e26c5c46e4431aa5
Princeyyyy <princemumo80@gmail.com>
2020-12-10 00:57:34 +0300

    added alert dialog authentication

--- app/src/main/java/com/example/todolist/HomeActivity.java
+++ app/src/main/java/com/example/todolist/HomeActivity.java
@@ -7,13 +7,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.zip.Inflater;
 
@@ -22,9 +28,17 @@ import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
 
-    @BindView(R.id.recyclerView) RecyclerView mrecyclerView;
-    @BindView(R.id.fab) FloatingActionButton mFloatingActionButton;
+    @BindView(R.id.recyclerView)
+    RecyclerView mrecyclerView;
+    @BindView(R.id.fab)
+    FloatingActionButton mFloatingActionButton;
     private Toolbar toolbar;
+
+    private DatabaseReference reference;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private String onlineUserId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +51,7 @@ public class HomeActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("To Do List");
+        mAuth = FirebaseAuth.getInstance();
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setReverseLayout(true);
@@ -44,6 +59,10 @@ public class HomeActivity extends AppCompatActivity {
 
         mrecyclerView.setHasFixedSize(true);
         mrecyclerView.setLayoutManager(linearLayoutManager);
+
+        mUser = mAuth.getCurrentUser();
+        onlineUserId = mUser.getUid();
+        reference = FirebaseDatabase.getInstance().getReference().child("tasks").child(onlineUserId);
 
 
         mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -63,8 +82,35 @@ public class HomeActivity extends AppCompatActivity {
         View view = layoutInflater.inflate(R.layout.input_file, null);
         myDialog.setView(view);
 
-        AlertDialog dialog = myDialog.create();
+        final AlertDialog dialog = myDialog.create();
         dialog.setCancelable(false);
+
+        final EditText task = view.findViewById(R.id.task);
+        final EditText description = view.findViewById(R.id.desc);
+        Button save = view.findViewById(R.id.saveBtn);
+        Button cancel = view.findViewById(R.id.cancelBtn);
+
+        cancel.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
+
+        save.setOnClickListener(v -> {
+            String mTask = task.getText().toString().trim();
+            String mDescription = description.getText().toString().trim();
+
+            if (TextUtils.isEmpty(mTask)) {
+                task.setError("Task Required");
+                return;
+            }
+            if (TextUtils.isEmpty(mDescription)) {
+                description.setError("Description Required");
+                return;
+            } else {
+
+            }
+            dialog.dismiss();
+        });
+
         dialog.show();
     }
 }
