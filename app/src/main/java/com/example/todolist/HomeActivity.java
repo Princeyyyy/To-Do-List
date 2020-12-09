d2a2cbb84b22c847f818d76afaab9fed2bd88649
Princeyyyy <princemumo80@gmail.com>
2020-12-10 00:23:51 +0300

    added alert dialog for adding tasks

--- app/src/main/java/com/example/todolist/HomeActivity.java
+++ app/src/main/java/com/example/todolist/HomeActivity.java
@@ -1,14 +1,70 @@
 package com.example.todolist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.zip.Inflater;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView) RecyclerView mrecyclerView;
+    @BindView(R.id.fab) FloatingActionButton mFloatingActionButton;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+
+        toolbar = findViewById(R.id.homeToolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("To Do List");
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        mrecyclerView.setHasFixedSize(true);
+        mrecyclerView.setLayoutManager(linearLayoutManager);
+
+
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask();
+            }
+        });
+
+    }
+
+    private void addTask() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+
+
+        View view = layoutInflater.inflate(R.layout.input_file, null);
+        myDialog.setView(view);
+
+        AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
