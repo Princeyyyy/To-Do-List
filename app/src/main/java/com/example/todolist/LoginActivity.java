7264dac2a07f13bd88016cbc953188ac02ca6b72
Princeyyyy <princemumo80@gmail.com>
2020-12-09 22:32:46 +0300

    added button intents for login and registration

--- app/src/main/java/com/example/todolist/LoginActivity.java
+++ app/src/main/java/com/example/todolist/LoginActivity.java
@@ -5,13 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.loginEmail) EditText mloginEmail;
+    @BindView(R.id.loginPassword) EditText mloginPassword;
+    @BindView(R.id.loginButton) Button mloginButton;
+    @BindView(R.id.signupScreen) Button msignupScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        msignupScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void onRegistrationClick(View View){
--- app/src/main/java/com/example/todolist/RegistrationActivity.java
+++ app/src/main/java/com/example/todolist/RegistrationActivity.java
@@ -5,13 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    @BindView(R.id.registrationEmail) EditText mregistrationEmail;
+    @BindView(R.id.registrationPassword) EditText mregistrationPassword;
+    @BindView(R.id.signupButton) Button msignupButton;
+    @BindView(R.id.loginScreen) Button mloginScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        ButterKnife.bind(this);
+
+        mloginScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void onLoginClick(View view){
