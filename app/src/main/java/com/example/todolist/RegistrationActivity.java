34ef0023ae8072f36f2ee2337747b8c5635caefa
Princeyyyy <princemumo80@gmail.com>
2020-12-09 23:04:45 +0300

    added registration functionality

--- app/src/main/java/com/example/todolist/RegistrationActivity.java
+++ app/src/main/java/com/example/todolist/RegistrationActivity.java
@@ -1,12 +1,21 @@
 package com.example.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,11 +27,17 @@ public class RegistrationActivity extends AppCompatActivity {
     @BindView(R.id.signupButton) Button msignupButton;
     @BindView(R.id.loginScreen) Button mloginScreen;
 
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        loader = new ProgressDialog(this);
 
         mloginScreen.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +45,43 @@ public class RegistrationActivity extends AppCompatActivity {
                 Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
+        });
+
+        msignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mregistrationEmail.getText().toString().trim();
+                String password = mregistrationPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    mregistrationEmail.setError("Email is Required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    mregistrationPassword.setError("Password Required");
+                }else{
+                    loader.setMessage("Registration in Progress");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+                    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+                                Intent intent = new Intent(RegistrationActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                                loader.dismiss();
+                            }else {
+                                String error = task.getException().toString();
+                                Toast.makeText(RegistrationActivity.this, "Registration Failed " + error , Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+                        }
+                    });
+                }
+            }
+
         });
     }
 
